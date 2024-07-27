4192ab4ba0788f7e4fcac285330580bdc5dff357
secgyu <64451661+secgyu@users.noreply.github.com>
2024-07-27 15:25:59 +0900

    Update Inventory.py

--- Adventure/Inventory.py
+++ Adventure/Inventory.py
@@ -1,4 +1,5 @@
 # 인벤토리 시스템
+from colorama import Fore
 
 inv = {
     "StructureKey": False,
@@ -83,18 +84,18 @@ def hasGalacticMap():
 
 
 def display():
-    print("******* 인벤토리 *******")
-    print("가진 동전은 ", numCoins(), "개입니다.")
+    print(Fore.CYAN+"******* 인벤토리 *******")
+    print(Fore.CYAN+"가진 동전은 ", numCoins(), "개입니다.")
     if hasStructureKey():
-        print("파랗게 빛나는 열쇠가 있습니다.")
+        print(Fore.CYAN+"파랗게 빛나는 열쇠가 있습니다.")
     if hasGalacticMap():
-        print("은하계 지도가 있습니다.")
+        print(Fore.CYAN+"은하계 지도가 있습니다.")
     if hasPlasmaShield():
-        print("플라스마 방패가 있습니다.")
+        print(Fore.CYAN+"플라스마 방패가 있습니다.")
     if hasLaserBlaster() == False and numGrenades() == 0:
-        print("아무런 무기도 없습니다.")
+        print(Fore.CYAN+"아무런 무기도 없습니다.")
     if hasLaserBlaster():
-        print("광선총이 있습니다.")
+        print(Fore.CYAN+"광선총이 있습니다.")
     if numGrenades() > 0:
         print(numGrenades(), "개의 퀀텀 수류탄이 있습니다.")
-    print("************************")
+    print(Fore.CYAN+"************************")
