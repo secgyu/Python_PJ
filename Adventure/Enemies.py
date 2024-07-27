199d67d29b0df08e66110da7523c823d6bb9288f
secgyu <64451661+secgyu@users.noreply.github.com>
2024-07-27 15:27:21 +0900

    Create Enemies.py

---
+++ Adventure/Enemies.py
@@ -0,0 +1,28 @@
+enemies = [
+    {
+        "id": "slug",
+        "description": "우주 괴물",
+        "strength": 10,
+        "damageMin": 1,
+        "damageMax": 3,
+        "defense": 2
+    },
+
+    {
+        "id": "eel",
+        "description": "방사능 장어",
+        "strength": 50,
+        "damageMin": 10,
+        "damageMax": 15,
+        "defense": 1
+    },
+
+    {
+        "id": "alien",
+        "description": "녹색 촉수의 외계인",
+        "strength": 25,
+        "damageMin": 5,
+        "damageMax": 10,
+        "defense": 3
+    }
+]
