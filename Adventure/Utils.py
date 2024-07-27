d52ef43fa79886cabc9d7677e1897a84d5de8f5f
secgyu <64451661+secgyu@users.noreply.github.com>
2024-07-27 15:25:37 +0900

    Update Utils.py

--- Adventure/Utils.py
+++ Adventure/Utils.py
@@ -1,10 +1,15 @@
-# 유틸리티 함수
+import random
+import Inventory as inv
+import Player as p
+from Items import items
+from colorama import Fore
+
 
 def getUserChoice(options):
     validInputs = ""
     for opt in options:
         validInputs += opt[0]
-        print(opt[0], "-", opt[1])
+        print(Fore.YELLOW + opt[0], "-", opt[1])
     prompt = "무엇을 하고 싶으세요? [" + validInputs + "]: "
     choice = ""
     done = False
@@ -31,3 +36,42 @@ def inputYesNo(text):
             return True
         elif x in ["N", "NO"]:
             return False
+
+
+def getItems():
+    result = []
+    for item in items:
+        i = []
+        i.append(item["key"])
+        i.append(item["description"] + " (" + str(item["cost"]) + ")")
+        result.append(i)
+    return result
+
+
+def randomEvent(freq):
+    return True if random.randrange(0, freq) == 0 else False
+
+
+def buyItem():
+    items_for_sale = getItems()
+    for item in items_for_sale:
+        print(f"{item[0]}: {item[1]}")
+    choice = input("구매할 아이템을 선택하세요: ").upper()
+    for item in items:
+        if item["key"] == choice:
+            if inv.numCoins() >= item["cost"]:
+                inv.takeCoins(item["cost"])
+                if choice == "H":
+                    p.addHealth(50)
+                elif choice == "B":
+                    inv.takeLaserBlaster()
+                elif choice == "G":
+                    inv.takeGrenades(3)
+                elif choice == "S":
+                    inv.takePlasmaShield()
+                elif choice == "L":
+                    p.addLife()
+                print(f"{item['description']}을(를) 구매했습니다!")
+            else:
+                print("동전이 부족합니다!")
+            break
